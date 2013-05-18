1aa0e8715fe1e71d264413b0e47e63759879d892
Guillaume Borne <guillaume.borne@gmail.com>
2013-05-18 11:46:02 +0200

    fix import foireux MapActivity

--- Pic_Trip/src/com/example/pic_trip/MainActivity.java
+++ Pic_Trip/src/com/example/pic_trip/MainActivity.java
@@ -1,26 +1,24 @@
 package com.example.pic_trip;
 
-import com.google.android.maps.MapActivity;
-import android.content.Context;
 import android.os.Bundle;
+ 
+import com.google.android.maps.MapActivity;
  
 public class MainActivity extends MapActivity {
  
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    
-	    //défini le vue XML utilisée
-	    setContentView(R.layout.activity_main);
-	  }
-	 
-	  @Override
-	  protected boolean isRouteDisplayed() {
-	    return true;
-	  }
-	 
-	  @Override
-	  protected boolean isLocationDisplayed() {
-	    return true;
-	  }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+ 
+  @Override
+  protected boolean isRouteDisplayed() {
+    return true;
+  }
+ 
+  @Override
+  protected boolean isLocationDisplayed() {
+    return false;
+  }
 }
