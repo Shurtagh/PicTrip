1afa0c18a2f6f89e446f825d23f198467b9c9914
Guillaume Borne <guillaume.borne@gmail.com>
2013-05-19 18:23:49 +0200

    affichage de la carte

--- Pic_Trip/src/com/example/pic_trip/MainActivity.java
+++ Pic_Trip/src/com/example/pic_trip/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.pic_trip;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
  
-import com.google.android.maps.MapActivity;
- 
-public class MainActivity extends MapActivity {
- 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
- 
-  @Override
-  protected boolean isRouteDisplayed() {
-    return true;
-  }
- 
-  @Override
-  protected boolean isLocationDisplayed() {
-    return false;
-  }
+public class MainActivity extends Activity {
+	private GoogleMap map;
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng PARIS = new LatLng(48.85661, 2.35222);
+	
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	    map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg")).showInfoWindow();
+	    map.addMarker(new MarkerOptions().position(PARIS).title("Paris")).showInfoWindow();
+	    map.addPolyline(new PolylineOptions()
+	     .add(HAMBURG, PARIS)
+	     .width(5)
+	     .color(Color.RED));
+	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 4));
+	  }
 }
